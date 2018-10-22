7cb5af1a4ff3886ab2691a969f0db589c668a8aa
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-10-22 09:45:04 -0300

    dipper: NoCutoutOverlay: Install on system

--- overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
+++ overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := DisplayCutoutNoCutout
+LOCAL_PACKAGE_NAME := DisplayCutoutNoCutout
 LOCAL_CERTIFICATE := platform
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
@@ -12,4 +12,4 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := NoCutoutOverlay
 LOCAL_SDK_VERSION := current
 
-include $(BUILD_RRO_PACKAGE)
+include $(BUILD_PACKAGE)
