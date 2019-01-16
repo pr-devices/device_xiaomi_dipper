5a9c3f29fb713f4c12d37f3219f838e6cf55637b
Henrique Pereira <pereirah2001@gmail.com>
2019-01-16 01:53:41 +0000

    dipper: CutoutOverlay: CleanUp
    * This commit also contains the fix for the visual bug when
      device is locked with cutout overlay is enabled
    
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
+++ overlay/packages/apps/overlays/NoCutoutOverlay/Android.mk
@@ -1,16 +1,13 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := DisplayCutoutNoCutout
-LOCAL_CERTIFICATE := platform
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := NoCutoutOverlay
 LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
 
 LOCAL_OVERRIDES_PACKAGES := DisplayCutoutEmulationCornerOverlay DisplayCutoutEmulationDoubleOverlay DisplayCutoutEmulationNarrowOverlay DisplayCutoutEmulationTallOverlay DisplayCutoutEmulationWideOverlay
 
