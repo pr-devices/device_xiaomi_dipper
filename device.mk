ca8b31ee4fd171958740646d33c8ed52c7cb3908
Henrique Pereira <pereirah2001@gmail.com>
2019-03-17 10:57:04 -0300

    [SQUASHED] dipper: Import new NFC configs from LOS tree
    * dipper: Import stock NFC configs
    
    * dipper: Align NFC configs with upstream
    
    * dipper: Remove system and vendor image dependency
    
    Signed-off-by: Henrique Pereira <pereirah2001@gmail.com>

--- device.mk
+++ device.mk
@@ -65,7 +65,8 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Wallpapers
 PRODUCT_PACKAGES += \
